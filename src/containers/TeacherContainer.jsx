9c0099fdc396d71be45ab87fdeaee18aa5a890eb
Kawamura Shintaro <s.kawamura@zanon.co.jp>
2016-11-26 16:40:58 +0900

    Add slide

--- src/containers/TeacherContainer.jsx
+++ src/containers/TeacherContainer.jsx
@@ -1,28 +1,42 @@
-import React, { Component, PropTypes } from 'react'
-import { Link } from 'react-router'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 class TeacherContainer extends Component {
   componentDidMount() {
   }
 
+  resizeSlide() {
+    // TODO: resize
+  }
+
   render() {
     return (
       <div>
+        {this.renderSlide()}
       </div>
     )
+  }
+
+  renderSlide() {
+    let height = window.screen.height;
+    let html = `<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQiTsrz3u4rkRBaORtaba5m0riMujZBm8h8Coph83Jz7bF0QSkq-zxlGqwXPt5j_7bfOJZt3b4xwLBU/embed?start=false&loop=false&delayms=3000" frameborder="0" width="100%" height="${height}" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>`;
+    let htmlObject = {__html: html};
+    return (
+      <div dangerouslySetInnerHTML={htmlObject}/>
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {}
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
   }, dispatch)
-}
+};
 
 export default connect(
   mapStateToProps,
